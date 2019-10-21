d7111f102964ddc9a0b9282594161e42f2c6b44d
Vincent-Ruby <vince.ruby@gmail.com>
2019-10-21 14:44:02 +0200

    second essai

--- BookMySeat/src/main/java/com/inti/formation/entity/User.java
+++ BookMySeat/src/main/java/com/inti/formation/entity/User.java
@@ -1,5 +1,13 @@
 package com.inti.formation.entity;
 
 public class User {
+	
+	private int id_user;
+	private String name;
+	private String firstName;
+	private Adress adress;
+	private String email;
+	private Handicap handicap;
+	private String username;
 
 }
