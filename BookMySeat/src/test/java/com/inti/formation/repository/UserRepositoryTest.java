95cb8f0e44cc80eaae65a3b9955cee865f5fc0a8
Vincent-Ruby <vince.ruby@gmail.com>
2019-10-22 12:44:48 +0200

    Premier test unitaire UserRepository réussi

--- BookMySeat/src/test/java/com/inti/formation/repository/UserRepositoryTest.java
+++ BookMySeat/src/test/java/com/inti/formation/repository/UserRepositoryTest.java
@@ -1,7 +1,6 @@
 package com.inti.formation.repository;
 
 import static org.junit.Assert.assertEquals;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
@@ -15,37 +14,52 @@ import com.inti.formation.entity.User;
 @RunWith(SpringRunner.class)
 @DataJpaTest
 public class UserRepositoryTest {
+
 	
+//	@Autowired
+//	private IUserRepository userRepo;
+//	private static final Logger LOGGER = LoggerFactory.getLogger(UserRepositoryTest.class);
+//	
+//	@Test
+//	public void givenEntityRepository_whenSaving() {
+//		LOGGER.info("------------------ Testing givenEntityRepository_whenSaving Method --------------");
+//		System.out.println(userRepo);
+//		User addedUser = userRepo.save(User.builder().name("Zidane").build());
+//		LOGGER.info("----------- User seved -------");
+//		LOGGER.info("----------- Searching for User -------");
+//		User foundUser = userRepo.getOne(addedUser.getId_user());
+//		LOGGER.info("----------- User found -------");
+//		LOGGER.info("----------- Verifying User -------");
+//		assertEquals(addedUser.getName(), foundUser.getName());
+//		LOGGER.info("----------- User verified -------");
+//	}
+//	
 	@Autowired
 	private IUserRepository userRepo;
 	private static final Logger LOGGER = LoggerFactory.getLogger(UserRepositoryTest.class);
-	
+
 	@Test
 	public void givenEntityRepository_saveMethod() {
 		LOGGER.info("____________Test UserRepository .save Method________________");
-		//given
+		// given
 		User dhia = User.builder().name("Dhia").build();
 		LOGGER.info("____________User saved________________");
 		userRepo.save(dhia);
-		//when
+		// when
 		LOGGER.info("____________Call User________________");
 		User calledDhia = userRepo.getOne(1);
-		//then
+		// then
 		LOGGER.info("____________Test equal________________");
-		assertEquals(dhia, calledDhia);
-		
+		assertEquals(dhia.getName(), calledDhia.getName());
+
 		/**
-	    Employee alex = new Employee("alex");
-	    entityManager.persist(alex);
-	    entityManager.flush();
-	 
-	    // when
-	    Employee found = employeeRepository.findByName(alex.getName());
-	 
-	    // then
-	    assertThat(found.getName())
-	      .isEqualTo(alex.getName());
-	     */
+		 * Employee alex = new Employee("alex"); entityManager.persist(alex);
+		 * entityManager.flush();
+		 * 
+		 * // when Employee found = employeeRepository.findByName(alex.getName());
+		 * 
+		 * // then assertThat(found.getName()) .isEqualTo(alex.getName());
+		 */
 	}
 
 }
