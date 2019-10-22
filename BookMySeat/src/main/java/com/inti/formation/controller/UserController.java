beeea991090c1724a82a3d4c6c38602bfae9975c
Robin Muller <robin.muller12@gmail.com>
2019-10-22 15:25:45 +0200

    pause

--- BookMySeat/src/main/java/com/inti/formation/controller/UserController.java
+++ BookMySeat/src/main/java/com/inti/formation/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,7 +29,7 @@ public class UserController {
 		return metier.add(u);
 	}
 	
-	@GetMapping(value="/update")
+	@PutMapping(value="/update")
 	public User update(@RequestBody User u) {
 		return metier.update(u);
 	}
