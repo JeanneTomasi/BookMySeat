064115fb4e1de15ba5c00f3b403265e62bb7b1a5
Robin Muller <robin.muller12@gmail.com>
2019-10-22 12:39:50 +0200

    connexion database

--- BookMySeat/src/main/java/com/inti/formation/repository/ITransportEntrepriseRepository.java
+++ BookMySeat/src/main/java/com/inti/formation/repository/ITransportEntrepriseRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.inti.formation.entity.TransportEntreprise;
+
 @Repository
 public interface ITransportEntrepriseRepository extends JpaRepository<TransportEntreprise, Integer>{
 
